d5906fe41ced071da0799fc9b650b2a193ac225f
2376858566@qq.com <2376858566@qq.com>
2019-09-19 13:45:13 +0800

    添加首页界面

---
+++ front/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="names">
+		<top-head></top-head>
+		
+
+
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	.el-carousel__item h3 {
+		color: #475669;
+		font-size: 18px;
+		opacity: 0.75;
+		line-height: 300px;
+		margin: 0;
+	}
+
+	.el-carousel__item:nth-child(2n) {
+		background-color: #99a9bf;
+	}
+
+	.el-carousel__item:nth-child(2n+1) {
+		background-color: #d3dce6;
+	}
+</style>
