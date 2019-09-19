2e61244747ef4b9ce0d81957aa441cbb119ee229
FightForYou <1228969071@qq.com>
2019-09-19 09:59:34 +0800

    实现商户登录，商户商品管理页面

--- front/src/App.vue
+++ front/src/App.vue
@@ -19,7 +19,7 @@ body {
 }
 
 #app {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
---
+++ front/src/components/GoodsShow.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="goodsShow">
+        <div class="gPhoto"></div>
+        <span class="gPrice">{{goods.gPrice}}</span>
+        <p class="gDescription">叽里呱啦1111111111111111111111111111111123123122222222222222222222</p>
+        <span class="shopName">小赤佬</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            props:[]
+        }
+    },
+    props:["goods"]
+}
+</script>
+
+<style lang="scss">
+    .goodsShow{
+        width: 240px;
+        height: 300px;
+        background-color: #d1f;
+        margin: 10px 10px ;
+        overflow-y: no;
+    }
+    .gPhoto{
+        width: 100%;
+        height: 70%;
+        background-color: blue;
+    }
+
+    .gPrice{
+        color: #FF4466;
+        font-size: 24px;
+    }
+
+    .gDescription{
+        margin: 10px 0;
+        color: #49F210;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+    }
+
+    .shopName{
+        color:#ccc;
+        font-size: 14px;
+    }
+</style>
