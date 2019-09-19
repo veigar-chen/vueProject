edf6121e7b6383fafa16e0b133cdeea022c69ff6
2376858566@qq.com <2376858566@qq.com>
2019-09-19 13:29:54 +0800

    添加首页路由

--- front/src/router.js
+++ front/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login from './views/Login.vue'
 import UserLogin from './components/UserLogin.vue'
 import UserReg from "@/components/UserReg.vue";
+import Home from './views/Home.vue'
 import UserInfo from "@/components/UserInfo.vue";
 import BossLogin from "@/components/BossLogin.vue";
 import Boss from './views/Boss.vue'
@@ -15,7 +16,12 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [{
+    routes: [
+		{
+		    path: '/',
+		    component: Home
+		},
+		{
         path: '/login',
         component: Login,
         children: [{
