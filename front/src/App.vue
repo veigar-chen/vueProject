cb31ea6842370c371d25a7ea47b1a889d4ad8df7
FightForYou <1228969071@qq.com>
2019-09-23 09:04:25 +0800

    新添保存用户登录状态，新添商品详情页面

--- front/src/App.vue
+++ front/src/App.vue
@@ -11,15 +11,29 @@
   margin: 0px;
   padding: 0px;
 }
-html,
-body {
-  width: 100%;
-  height: 100%;
-  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+i {
+  font-style: normal;
+}
+
+body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, div, dl, dt, dd, ul, ol, li, pre, form, fieldset, legend, button, input, textarea, option, select, th, td, img, background {
+    border: none;
+    border: 0;
+    margin: 0;
+    padding: 0;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    color: #555555;
+    font: 14px "\5fae\8f6f\96c5\9ed1",Helvetica,PingFang SC,Tahoma,Arial,sans-serif;
+    outline: none;
+    resize: none;
+    word-break: break-all;
 }
 
 #app {
   width: 100%;
   height: 100vh;
+}
+
+a {
+    color: #555555;
 }
 </style>
